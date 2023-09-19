14ca47c6744029d358c32195fa8e32e9ae0e63c8
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-19 14:42:09 +0000

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNunmber = 34567890934343133444443n
+
+
+
+// Reference (Non- primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj = {
+    name : "hitesh",
+    age: 22,
+}
+
+const myFunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNunmber);
