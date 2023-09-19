9935c8079b8fe097de0728e2814f0b97777d1551
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-19 14:55:10 +0000

    update on datatype summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -32,4 +32,30 @@ const myFunction = function (){
     console.log("Hello World");
 }
 
-console.log(typeof bigNunmber);
+//console.log(typeof bigNunmber);
+
+
+// ***********************************************************
+
+// stack (Primitive), Heap(Non-primitive)
+
+let myYoutubename = "praminghimiredotcom"
+
+let anotherName = myYoutubename
+anotherName = "chaiaourcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "test@test.com"
+
+}
+
+let userTwo= userOne
+
+userTwo.email = "pramin100@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
