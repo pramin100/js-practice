1782d05349a71637dbbcefa10c2139ec4b18804c
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-19 15:49:00 +0000

    number and maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,26 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8955
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-NP'));
+
+// ********************* Maths *********************
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,6,1));
+console.log(Math.random());
