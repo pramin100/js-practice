d7ba5559eeeba243cf4573c37ed288f486e1cd97
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-20 16:13:09 +0000

    scopes

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,47 @@
+//var c = 300
+let a = 300
+if(true){
+    let a =10
+    const b = 20
+    var c = 30
+   //console.log("Inner: ",a);
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one() {
+    const username = "Pramin"
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website)
+    two()
+}
+//one()
+
+if(true){
+    const username = "Pramin"
+    if(username === "Pramin"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+// ++++++++++++ Interesting +++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2
+}
+
