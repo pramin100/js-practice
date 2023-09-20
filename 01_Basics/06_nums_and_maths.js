bd7ed8cb8a9ab10846222dd5079bbb6560803e26
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-20 06:43:13 +0000

    nums and math

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -23,4 +23,12 @@
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4,5,6,1));
+
 console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+ min);
