fab867ca6e1fd1aabeaaa0f216328dd20bce575f
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-20 15:47:09 +0000

    01functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -30,4 +30,32 @@ function loginUserMessage(username = "Sam"){
     
 }
 
-console.log(loginUserMessage("Pramin"));
+//console.log(loginUserMessage("Pramin"));
+
+function calculateCartPrice(val1,val2, ...num1){
+    return num1;
+}
+//console.log(calculateCartPrice(200,400,500,2000,100));
+
+const user ={
+    username : "Pramin",
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 400
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
