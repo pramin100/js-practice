d45fb85cf1cb31a3f135ec453a17f63b3eff2b1c
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-20 08:42:56 +0000

    04Objects

---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,52 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser);
+
+const regularUser = {
+    email : "someone@gmail.com",
+    fullname : {
+        userFullname : {
+            firstName : "Pramin",
+            lastname : "Ghimire"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userFullname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+//const obj3 = {obj1, obj2} // cannot use it will show issue
+
+//const obj3 = Object.assign({},obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+
+//console.log(obj3);
+
+const users = [
+    {
+        id: 1, 
+        email : "h@gmail.com"
+    },
+    {
+        id: 2, 
+        email : "g@gmail.com"
+    }
+]
+
+//console.log(users[1].email)
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
