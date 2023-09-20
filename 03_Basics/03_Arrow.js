90df86a70147eca6668d3c4368117fb54038e4d1
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-20 17:45:18 +0000

    arrows

---
+++ 03_Basics/03_Arrow.js
@@ -0,0 +1,32 @@
+const user = {
+    username : "Pramin",
+    price: 199,
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this)
+    }
+
+
+}
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+//console.log(this)
+
+// function chai(){
+//     console.log(this);
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = ()=>{
+//     let username = "Pramin"
+//     console.log(this);
+// }
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(addTwo(3,4));
