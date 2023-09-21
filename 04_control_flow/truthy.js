ee5724af583f309b409f47364044d37884f2ebcd
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-21 17:53:35 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+// const userEmail =  "p@pramin.com"
+
+// if(userEmail){
+//     console.log("Got user Email");
+// } else {
+//     console.log("Don't Have user email");
+// }
+
+// falsy values : false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values : "0", 'flase', " ", [], {}, function(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+// Nullish coalescing Operation (??) : null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+
+// val1 = null ?? 10 ?? 15
+
+
+// console.log(val1);
+
+
+// Terniary Operator
+
+//condition ? true : false 
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
