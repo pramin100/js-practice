777a9c4d2fda5e583407724899c33cc581fa3818
pramin100 <57341158+pramin100@users.noreply.github.com>
2023-09-22 13:33:32 +0000

    threefour

---
+++ 05_Iterations/four.js
@@ -0,0 +1,26 @@
+const myObject ={
+    js : "Javascipt",
+    cpp : "C++",
+    rb : "ruby",
+    swift : "Swift by apple"
+ }
+
+ for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+ }
+
+// const programming =["js","rb","py","java"]
+// for (const key in programming) {
+//         console.log(programming[key]);
+//     }
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('NP',"Nepal")
+// map.set('USA',"United State of America")
+
+// for (const key in map) {
+//     console.log([key]);
+        
+//     }
+
